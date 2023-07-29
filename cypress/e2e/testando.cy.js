3f5089a5aa9b8dfbb68205ef21a6d58bdc6d0527
Eron Moraes <eron.moraes7@gmail.com>
2023-07-29 10:37:18 -0300

    register

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a', 'Register now').click();
+    cy.get('[formcontrolname="email"]').type('eron.moraes7@email.com.br');
+    cy.get('[formcontrolname="fullName"]').type('Eron Moraes');
+    cy.get('[formcontrolname="userName"]').type('eronmoraes');
+    cy.get('[formcontrolname="password"]').type('qwe123');
+    cy.contains('button', 'Register').click();
   })
 })
