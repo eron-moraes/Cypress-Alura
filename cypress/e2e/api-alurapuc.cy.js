c312c8bb0f99bc4473752354358db09ff255b0e5
Eron Moraes <eron.moraes7@gmail.com>
2023-08-06 22:47:54 -0300

    page object

--- cypress/e2e/api-alurapuc.cy.js
+++ cypress/e2e/api-alurapuc.cy.js
@@ -1,14 +1,21 @@
-describe('Api Alurapic', () => {
-    it('Dados da API', () => {
+describe('Buscar fotos e dados', () => {
+
+    // código comentado omitido…
+    
+        it('buscar fotos do usuário', () => {
+          
+          const tempoEsperado = Math.random() * 50;
+    
             cy.request({
-                method: 'POST',
-                url: 'http://localhost:3000/user/login',
-                body: Cypress.env()
+                method: 'GET' ,
+                url: 'http://localhost:3000/carowl/photos'
             }).then((res) => {
                 expect(res.status).to.be.equal(200)
                 expect(res.body).is.not.empty
-                expect(res.body).to.have.property('id')
-                expect(res.body.id).to.be.equal(4)
+                expect(res.body[0]).to.have.property('description')
+                expect(res.body[0].description).to.be.equal('porquinho')
+                expect(res.duration).to.be.lte(tempoEsperado)
             })
+        })
     })
-})
+    
